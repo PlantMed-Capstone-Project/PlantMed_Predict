4b320ac929496ed60362cf0094c61a5113196bf3
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2024-01-15 20:38:56 +0700

    feat: hosting predict endpoint

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 """
-This file is to test Cyclic's hosting capabilities
+This file is to test hosting capabilities
 """
 
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, Response
+from ultralytics import YOLO
+from PIL import Image
 from uuid import uuid4
 import io
-import uvicorn
 
 app = FastAPI()
+model = YOLO("models/best.pt")
+
+
 origins = ["*"]
 
 app.add_middleware(
@@ -33,3 +37,18 @@ async def upload_image(file: UploadFile = File(...)):
     contents = await file.read()
 
     return StreamingResponse(io.BytesIO(contents), media_type="image/jpeg")
+
+
+@app.post("/predict", tags=["Predict"])
+async def image_detection(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    image = Image.open(io.BytesIO(image_bytes))
+
+    results = model.predict(source=image, conf=0.5)
+    im_array = results[0].plot()
+    img = Image.fromarray(im_array[..., ::-1])
+    img_bytes = io.BytesIO()
+    img.save(img_bytes, format="PNG")
+    img_bytes = img_bytes.getvalue()
+
+    return Response(content=img_bytes, media_type="image/jpg")
