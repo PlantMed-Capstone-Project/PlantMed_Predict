1758c284c6a81b9c19753c95749c37dc32588d1c
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2024-04-07 12:09:58 +0700

    fixed: using yolo for predict first

--- database/config.py
+++
@@ -1,1 +0,0 @@
-connection_string = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=PlantMed;UID=sa;PWD=123456;"
--- main.py
+++ main.py
@@ -1,12 +1,57 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import Response
 from ultralytics import YOLO
 import io
 from PIL import Image
+import pyodbc
+from collections import Counter
 
 app = FastAPI()
 model = YOLO("models/best.pt")
+
+conn = pyodbc.connect(
+    "DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=PlantMed;UID=sa;PWD=123456;"
+)
+
+sql = conn.cursor()
+
+
+def get_by_name(name: str) -> dict:
+    sql.execute("SELECT * FROM Plant WHERE name = ?", (name,))
+    row = sql.fetchone()
+    response = {
+        "id": row.id.lower(),
+        "name": row.name,
+        "internationalName": row.international_name,
+        "surName": row.sur_name,
+        "placeOfBirth": row.place_of_birth,
+        "shopBase": row.shop_base,
+        "origin": row.origin,
+        "usage": row.usage,
+    }
+    return response
+
+
+def format(results: list) -> float:
+    if not results:
+        return 0.0
+
+    map = {}
+    for rs in results:
+        if rs in map:
+            map[rs] += 1
+        else:
+            map[rs] = 1
+
+    if not map:
+        return 0.0
+
+    max_value = max(map.values())
+
+    percentage = (max_value / len(results)) * 100
+    return round(percentage, 2) if percentage else 0.0
+
 
 origins = ["*"]
 
@@ -25,16 +70,41 @@ def root():
     return {"status": "OK", "message": "Hello World in AI PlantMed"}
 
 
-@app.post("/predict", tags=["Predict"])
+@app.post("/predict/yolo", tags=["Predict"])
 async def image_detection(file: UploadFile = File(...)):
-    image_bytes = await file.read()
-    image = Image.open(io.BytesIO(image_bytes))
+    try:
+        image = Image.open(io.BytesIO(await file.read()))
+        results = model.predict(source=image, conf=0.5)
 
-    results = model.predict(source=image, conf=0.5)
-    im_array = results[0].plot()
-    img = Image.fromarray(im_array[..., ::-1])
-    img_bytes = io.BytesIO()
-    img.save(img_bytes, format="PNG")
-    img_bytes = img_bytes.getvalue()
+        class_result = []
+        for result in results:
+            for box in result.boxes:
+                class_id = int(box.data[0][-1])
+                class_result.append(model.names[class_id])
 
-    return Response(content=img_bytes, media_type="image/jpg")
+        counter = Counter(class_result)
+        most_common = counter.most_common(1)
+        name = most_common[0][0]
+
+        plant = get_by_name(name)
+        acc_result = f"{format(class_result)}%"
+
+        response = {"accuracy": acc_result, "plant": plant}
+
+        return JSONResponse(response)
+    except:
+        raise HTTPException(status_code=500, detail="An error occurred in predict")
+
+
+@app.post("/predict/tf", tags=["Predict"])
+async def predict(file: UploadFile = File(...)):
+    response = {}
+    return response
+
+
+async def save(data):
+    try:
+        pass
+    except:
+        raise HTTPException(status_code=500, detail="")
+    pass
